5c6c56b08944625e5108aa02ffe84b23d028f339
Ben Ong <ben_ong@yahoo.com>
2023-01-20 17:25:01 -0800

    Initial commit

---
+++ static/js/bookmark.js
@@ -0,0 +1,32 @@
+const btnBookmarks = document.querySelectorAll(".bookmark");
+
+for (const btnBookmark of btnBookmarks) {
+   btnBookmark.addEventListener("click", () => {
+      console.log(`${btnBookmark.id} clicked!`);
+      const location = {
+         locationId: btnBookmark.id
+      }
+
+               const fill = document.querySelector(".bookmark-icon").getAttribute('fill') 
+
+               if (fill == 'None') {
+                  document.querySelector(".bookmark-icon").setAttribute('fill', 'blue');
+               } else {
+                  document.querySelector(".bookmark-icon").setAttribute('fill', 'None');
+               }
+
+      //-- Fetch: 
+      // fetch('/add_bookmark', {
+      //    method: 'POST',
+      //    body: JSON.stringify(location),
+      //    headers: {
+      //      'Content-Type': 'application/json',
+      //    },
+      // }).then((res) => res.json())
+      // }).then(data => {
+      //    if (data == 'Success') {
+      //       // Update DOM   
+      //    }
+      // });
+   });
+}
